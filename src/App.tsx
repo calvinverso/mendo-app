6c61c440e564ca752735dd839f314b4c1671e5e9
Calvin <calvin_espinoza@hotmail.com>
2019-10-11 23:42:38 -0600

    testing top tracks request

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,90 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import queryString from 'query-string';
 //import axios from 'axios';
 //import { Link, BrowserRouter } from 'react-router-dom'
 
 
-const App: React.FC = () => {
+interface Props { }
 
-  /*
-  const [user, setUser] = React.useState(null);
-  React.useEffect(() => {
-    axios.get('/api')
-      .then(response => {
-        setUser(response.data)
-      })
-  }, [])
+interface State {
+  name: string,
+  userData: { display_name: string },
+  access_token: string
+};
 
-  function handleLogin() {
-    axios.get('/login').then(res => console.log("did it do something?"))
+
+class App extends React.Component<Props, State> {
+
+  state: State = {
+    name: 'No Name',
+    userData: null,
+    access_token: ''
+  };
+  componentDidMount() {
+    this.getInfo();
+    //console.log(this.state.userData)
   }
-*/
-function handleLogin() {
-  /*
-  if (process.env.NODE_ENV === "production" ) {
-    window.location.href = "http://mendo-server.herokuapp.com/login"
-  } else { 
-    window.location.href = "http://localhost:8888/login"
-  }*/
-  window.location.href = "http://mendo-server.herokuapp.com/login"
-}
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hey, user
+  getInfo() {
+    let access_token = queryString.parse(window.location.search).access_token;
+    console.log(access_token);
+    if (access_token) {
+      this.setState({ access_token: access_token.toString() })
+      fetch("https://api.spotify.com/v1/me", {
+        headers: {
+          'Authorization': "Bearer " + access_token
+        }
+      }).then((res) => res.json()).then(data => {
+        this.setState({ userData: data });
+        console.log(data)
+      })
+
+
+      fetch("https://api.spotify.com/v1/me/top/tracks/", {
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+          'Authorization': "Bearer " + access_token
+        }
+      }).then((res) => console.log(res)).then(data=>console.log(data));
+      
+    }
+
+  }
+
+  handleLogin() {
+    if (process.env.NODE_ENV) {
+      window.location.href = "http://localhost:8888/login"
+    } else {
+      window.location.href = "http://mendo-server.herokuapp.com/login"
+    }
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            hey, user
         </p>
+          {this.state.userData ? <p>Hey que tal, {this.state.userData.display_name}</p> : <button type="button" className="btn btn-info" onClick={this.handleLogin}>Login</button>}
 
-        <button type="button" className="btn btn-info" onClick={handleLogin}>Login</button>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
